2267086970dec3fa64b25ee70eacae6958e2fcce
parkjm415 <pjm605@gmail.com>
2017-03-12 19:36:22 -0400

    removed www from gitignore to support heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var express = require('express'),
-app = express();
-app.use(express.static('www'));
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+var app = express();
+app.use(bodyParser.json());
 app.set('port', process.env.PORT || 5000);
+app.use(cors());
+app.use(express.static('www'));
+
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
